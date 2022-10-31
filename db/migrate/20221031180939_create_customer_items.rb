8e19eb43425818813e691efa7d44f1e93e523662
Matisse Mallette <matissemallette@gmail.com>
2022-10-31 11:36:58 -0700

    Fix migration issue

---
+++ db/migrate/20221031180939_create_customer_items.rb
@@ -0,0 +1,6 @@
+class CreateCustomerItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :customer_items do |t|
+    end
+  end
+end
---
+++ db/migrate/20221031182513_add_customer_ref_to_customer_items.rb
@@ -0,0 +1,5 @@
+class AddCustomerRefToCustomerItems < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :customer_items, :customer, foreign_key: true
+  end
+end
